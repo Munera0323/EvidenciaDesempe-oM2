fa46184b74b7ad729bd97508beefaa8772a03c0a
Munera0323 <muneragarcia123@gmail.com>
2025-05-07 18:51:30 -0500

    se añadieron unas variables

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.lang.runtime.SwitchBootstraps;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+
+        /*Contextualización:
+        El centro de eventos Royal ofrece alquiler de espacios y servicios adicionales con condiciones
+        especiales según el evento.
+        Menú de opciones:
+        1. Alquilar salón
+        2. Alquilar área al aire libre
+        3. Contratar servicios adicionales
+        Condiciones:
+        Opción 1 - Alquilar salón:
+        Solicitar duración del evento en horas y día de la semana.
+• Si el evento dura más de 5 horas, aplicar 15% de descuento.
+• Si es en sábado o domingo, aplicar recargo del 20%.
+
+        Opción 2 - Alquilar área al aire libre:
+        Solicitar tipo de evento y número de personas.
+• Eventos infantiles con más de 20 personas obtienen 10% de descuento.
+• Eventos sociales con más de 50 personas tienen recargo del 10%.
+
+        Opción 3 - Contratar servicios adicionales:
+        Solicitar el servicio.
+• Sonido e iluminación básica no tienen costo.
+• Catering y decoración tienen tarifa adicional.*/
+        String alquilerEspacio="alquilerEspacio";
+
+        System.out.println("Alquiler de espacios para eventos Royal: ");
+        System.out.println("/n-1 Alquilar salon,/n-2 Alquilar area al aire libre,/n-3contratar servicios adicionales");
+        switch (alquilerEspacio){
+            case "1" {
+                S
+            }
+            break;
+        }
+
+
+    }
+}
